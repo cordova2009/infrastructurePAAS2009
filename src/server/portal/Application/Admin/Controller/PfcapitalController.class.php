85a1d02bbe528b81a3fa471b63fd989e66cf029b
xuebingwang <406964108@qq.com>
2015-11-22 10:50:39 +0800

    交易管理功能完成

--- src/server/portal/Application/Admin/Controller/PfcapitalController.class.php
+++ src/server/portal/Application/Admin/Controller/PfcapitalController.class.php
@@ -15,6 +15,72 @@ namespace Admin\Controller;
 class PfcapitalController extends AdminController {
 
     /**
+     * 交易列表
+     */
+    public function dealindex(){
+
+        $where = [
+        ];
+
+        $order_id = I('order_id','','trim');
+        if(!empty($order_id)){
+            $where['a.order_id'] = ['like', '%'.(string)$order_id.'%'];
+        }
+
+        $flow_direction = I('flow_direction','','trim');
+        if(!empty($flow_direction)){
+            $where['a.flow_direction'] = $flow_direction;
+        }
+
+
+        $type = I('type','','trim');
+        if(!empty($type)){
+            $where['a.type'] = $type;
+        }
+
+        $order_id = I('order_id','','trim');
+        if(!empty($order_id)){
+            $where['a.order_id'] = ['like', '%'.(string)$order_id.'%'];
+        }
+
+        $prefix = C('DB_PREFIX');
+        $model = M()->table($prefix.'project_payment_account_order a');
+
+        $fields = 'a.*';
+
+        $list = $this->lists($model,$where,'a.insert_time desc',$fields);
+        $this->assign('list',$list);
+
+        $this->meta_title = '交易管理';
+
+        $this->assign('type_array',['SGC'=>'平台方收工程款','FGC'=>'平台方付工程款']);
+        $this->assign('flow_array',['IN#'=>'转入','OUT'=>'转出']);
+        $this->display();
+    }
+
+    /**
+     * 交易详情
+     */
+    public function viewdeal($order_id=''){
+
+        $where = [
+            'order_id'=>$order_id
+        ];
+
+        $prefix = C('DB_PREFIX');
+        $model = M()->table($prefix.'project_payment_account_order a');
+
+        $fields = 'a.*';
+
+        $this->assign('item',$model->where($where)->field($fields)->find());
+        $this->meta_title = '交易管理详情';
+
+        $this->assign('type_array',['SGC'=>'平台方收工程款','FGC'=>'平台方付工程款']);
+        $this->assign('flow_array',['IN#'=>'转入','OUT'=>'转出']);
+        $this->display();
+    }
+
+    /**
      * 正在招标中的列表
      */
     public function danbaoindex(){

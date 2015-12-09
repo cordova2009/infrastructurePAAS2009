a05f84c600642560f2d29d7b7cc44d24f6e639cc
xuebingwang <406964108@qq.com>
2015-12-09 22:16:15 +0800

    后台bug修复
    0000599

--- src/server/portal/Application/Admin/View/ace/Ztglobject/yzbindex.php
+++ src/server/portal/Application/Admin/View/ace/Ztglobject/yzbindex.php
@@ -13,7 +13,7 @@
                         <label>项目名称
                             <input type="text" class="search-input" name="object_name" value="{:I('object_name')}">
                         </label>
-                        <label>项目类别
+                        <label>工程类别
                             <?=form_dropdown('industry_id',[''=>'---全部---']+$industry,I('industry_id'))?>
                         </label>
                         <label>
@@ -30,9 +30,11 @@
 			    <thead>
 			        <tr>
                         <th class="">操作</th>
+                        <th class="">项目编号</th>
+                        <th class="">项目名称</th>
                         <th class="">中标公司</th>
-                        <th class="">项目编号/名称</th>
-                        <th class="">标的估价</th>
+                        <th class="text-right">中标金额</th>
+                        <th class="text-right">标的估价</th>
                         <th class="">工程类别</th>
                         <th class="">创建时间</th>
                         <th class="">开标日期</th>
@@ -46,13 +48,15 @@
                         <td>
                             <a data-rel="tooltip" data-placement="bottom" title="查看中标详情" href="{:U('viewzhongbiao?id='.$vo['bid'])}" class="ui-pg-div"><span class="ui-icon icon-zoom-in blue"></span></a>
                         </td>
+						<td><a data-rel="tooltip" data-placement="bottom" title="查看标的详情" href="{:U('viewzb',['object_id'=>$vo['object_id'],'active'=>'b'])}">{$vo.object_no}</a></td>
+                        <td><?=$vo['object_name']?></td>
                         <td>
                             <a data-rel="tooltip" data-placement="bottom" title="查看中标详情" href="{:U('viewzhongbiao?id='.$vo['bid'])}">
                                 <?=$vo['company_name']?>
                             </a>
                         </td>
-						<td><a data-rel="tooltip" data-placement="bottom" title="查看标的详情" href="{:U('viewzb',['object_id'=>$vo['object_id'],'active'=>'b'])}">{$vo.object_no}/<?=$vo['object_name']?></a></td>
-                        <td><?=price_format($vo['evaluation_amount'])?></td>
+                        <td class="text-right"><?=price_format($vo['bid_amount'])?></td>
+                        <td class="text-right"><?=price_format($vo['evaluation_amount'])?></td>
                         <td><?=$industry[$vo['industry_id']]?></td>
 						<td><?=$vo['insert_time']?></td>
 						<td>{$vo.bid_open_date}</td>
@@ -60,7 +64,7 @@
 					</tr>
 					</volist>
 					<else/>
-					<td colspan="9" class="text-center"> aOh! 暂时还没有内容! </td>
+					<td colspan="11" class="text-center"> aOh! 暂时还没有内容! </td>
 					</notempty>
 				</tbody>
             </table>

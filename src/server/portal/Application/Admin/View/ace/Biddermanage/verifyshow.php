ec5723ee56681572f97542c815bb67094ba4cb98
wangyf <276180638@qq.com>
2015-12-15 20:29:56 +0800

    bug修复

--- src/server/portal/Application/Admin/View/ace/Biddermanage/verifyshow.php
+++ src/server/portal/Application/Admin/View/ace/Biddermanage/verifyshow.php
@@ -95,7 +95,7 @@ img{max-width:400px;}
 			<td><span style="color:#999;padding-right:8px;"><input type="checkbox" value="Y" name="legal_person_idcard_back_url"> 审核通过</span><input type="text" name="legal_person_idcard_back_url_msg" placeholder="输入审核未通过原因"></td>
 		</tr>
 		<tr>
-			<td ><span style="color:#999;padding-right:8px;">法人授权书:</span><img src="{$item.legal_person_authority_book|imageView2}"/></td>
+			<td ><span style="color:#999;padding-right:8px;">法人授权书:</span><?php if(!empty($item['legal_person_authority_book'])){?> <img src="<?=imageView2($item['legal_person_authority_book'])?>" /><?php }?></td>
 			<td><span style="color:#999;padding-right:8px;"><input type="checkbox" value="Y" name="legal_person_authority_book"> 审核通过</span><input type="text" name="legal_person_authority_book_msg" placeholder="输入审核未通过原因"></td>
 		</tr>
 	 </tbody>

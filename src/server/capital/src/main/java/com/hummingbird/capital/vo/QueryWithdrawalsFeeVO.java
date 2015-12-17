e9cdc0070abe57775ec629fc062825a73b10a1c0
cordova2009 <875593212@qq.com>
2015-12-17 15:59:51 +0800

    ok

---
+++ src/server/capital/src/main/java/com/hummingbird/capital/vo/QueryWithdrawalsFeeVO.java
@@ -0,0 +1,14 @@
+package com.hummingbird.capital.vo;
+
+public class QueryWithdrawalsFeeVO {
+	private Long amount;
+
+	public Long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+	
+}
